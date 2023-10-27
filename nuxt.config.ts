b35359cc7bd87f0df4dec0c65a0cfcc084124fbc
Tapan Padaliya <tapan@7span.com>
2023-10-27 09:43:46 +0530

    pwa init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,62 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/pwa"],
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+  pwa: {
+    manifest: {
+      name: "Nuxt3 PWA Test",
+      short_name: "Nuxt3 PWA",
+      description: "Testing Nuxt3 PWA",
+      icons: [
+        {
+          src: "/assets/icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
+        },
+        {
+          src: "/assets/icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
 });
